381d8c5737d3b1315e5300d77b41bdf7bd607bbc
naitik-raj17 <naitikr.ug24.cs@nitp.ac.in>
2025-10-25 10:56:41 +0530

    Day 2
    AuthRoutes , model Schema, used becrypt to hash password and jwt to hide token

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,27 @@
+// const mongoose = require('mongoose');
+
+// function connectDB(){
+//     mongoose.connect("mongodb://localhost:27017/")
+//     // mongoose.connect("mongodb://127.0.0.1:27017/")
+//     .then(()=>{
+//         console.log("MongoDB connected");
+//     })
+//     .catch((err)=>{
+//         console.log("MongoDB connection error:",err);
+//     })
+// }
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect("mongodb://localhost:27017/food-view")
-    .then(()=>{
-        console.log("MongoDB connected");
+    // Add database name and connection options
+    mongoose.connect("mongodb://localhost:27017/food-view?family=4") 
+   .then(()=>{
+        console.log("MongoDB connected successfully");
     })
     .catch((err)=>{
-        console.log("MongoDB connection error:",err);
+        console.log("MongoDB connection error:", err.message);
     })
 }
 
