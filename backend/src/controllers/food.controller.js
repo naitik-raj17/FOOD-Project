9527c46bdbc162e8b694d7ae08ba679fdd773702
naitik-raj17 <naitikr.ug24.cs@nitp.ac.in>
2025-11-08 16:24:08 +0530

    Day 7
    setting up imagekit for storage services,
     error at uploading video

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,10 +1,20 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.services');
+const { v4:uuid} = require("uuid")
+ 
+async function createFood(req, res) {
+    try {
+        console.log(req.foodPartner);
+        console.log(req.body);
+        console.log(req.file);
 
-async function createFood(req,res){
-    console.log(req.foodPartner)
-    console.log(req.body)
-    console.log(req.file)
-    res.send("food item created")
+        const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid());
+        console.log(fileUploadResult);
+        res.send("food item created");
+    } catch (error) {
+        console.error('Error creating food item:', error);
+        res.status(500).send("Error creating food item");
+    }
 }
 
 module.exports = {
---
+++ backend/src/services/storage.services.js
@@ -0,0 +1,24 @@
+const Imagekit = require("@imagekit/nodejs");
+
+const imagekit = new Imagekit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+async function uploadFile(file, fileName) {
+    try {
+        const result = await imagekit.upload({
+            file: file,
+            fileName: fileName,
+        });
+        return result;
+    } catch (error) {
+        console.error('ImageKit upload error:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    uploadFile
+}
